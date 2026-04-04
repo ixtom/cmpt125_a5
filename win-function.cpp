1679f7f00f7da94fb6f15d646486887fbd239974
ixtom <189190919+ixtom@users.noreply.github.com>
2026-04-03 21:13:04 -0700

    added row win condition

--- win-function.cpp
+++ win-function.cpp
@@ -7,3 +7,42 @@ using namespace std;
 
 // win functions to call to check for win conditions
 // - Mengna
+struct coords{
+    int col;
+    int row;
+    int player; //1 or 2
+};
+
+bool const row_con(vector<vector<int>> grid, int row, int player){
+    for (int i=0;i<4;i++){ //checks column 0-3 to see if they have a chip
+        if (grid[i][row]==player){
+            int count;
+            for (int r=i+1;r<i+4;r++){
+                if (grid[r][row]!=player){
+                    break;
+                }
+                count++;
+            }
+            if (count==3){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool const win_con(vector<vector<int>> grid, coords move){ //vector grid is the current grid, coords move is the col and row ints of the last move. Remember to calculate this.
+    
+}
+
+int main(){
+    vector<vector<int>> verticalWin = {
+    {1, 1, 1, 1, 0, 0}, // Column 0: Four 1s stacked from bottom
+    {0, 0, 0, 0, 0, 0}, // Column 1
+    {0, 0, 0, 0, 0, 0}, // Column 2
+    {0, 0, 0, 0, 0, 0}, // Column 3
+    {0, 0, 0, 0, 0, 0}, // Column 4
+    {0, 0, 0, 0, 0, 0}, // Column 5
+    {0, 0, 0, 0, 0, 0}  // Column 6
+};
+}
