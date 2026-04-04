32252731b388ec852dbf16448628e7862a37c77d
ixtom <189190919+ixtom@users.noreply.github.com>
2026-04-03 17:31:37 -0700

    added win function file

---
+++ win-function.cpp
@@ -0,0 +1,9 @@
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// win functions to call to check for win conditions
+// - Mengna
